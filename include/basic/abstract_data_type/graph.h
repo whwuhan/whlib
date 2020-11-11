4643d5a405621a813a2f860b7a0b19c473b07714
whwuhan <695549978@qq.com>
2020-11-11 21:51:42 +0800

    修改图的表示方式

--- include/basic/abstract_data_type/graph.h
+++ include/basic/abstract_data_type/graph.h
@@ -15,70 +15,77 @@ namespace wh{
             class Graph{
             public:
                 Graph();
-                Graph(const std::vector<T>& nodes, const std::vector< std::pair<unsigned int, unsigned int> >& edges);
+                //Graph(const std::vector<T>& nodes, const std::vector< std::pair<unsigned int, unsigned int> >& edges);
                 
-                std::vector<T> nodes;//图的节点
-                std::vector< std::pair<unsigned int, unsigned int> > edges;//图的边
+                Eigen::MatrixXd points;
+                Eigen::MatrixXi edges;
+                // std::vector<T> nodes;//图的节点
+                // std::vector< std::pair<unsigned int, unsigned int> > edges;//图的边
                 unsigned int size;//图的点的个数
 
                 //get函数
-                std::vector<T>& get_nodes();
-                std::vector< std::pair<unsigned int, unsigned int> >& get_edges();
-                unsigned int get_size();
+                // std::vector<T>& get_nodes();
+                // std::vector< std::pair<unsigned int, unsigned int> >& get_edges();
+                // unsigned int get_size();
 
                 //专用函数
                 //将节点数据转化成Eigen矩阵
-                Eigen::MatrixXd nodes_data_to_matrix();
+                // Eigen::MatrixXd nodes_data_to_matrix();
 
                 //将图的边转化成邻接矩阵
-                Eigen::MatrixXd get_adjacent_matrix();
+                // Eigen::MatrixXd get_adjacent_matrix();
             };
 
             //构造函数
+            // template <typename T>
+            // Graph<T>::Graph():nodes(),edges(),size(0){
+
+            // }
+
             template <typename T>
-            Graph<T>::Graph():nodes(),edges(),size(0){
+            Graph<T>::Graph():points(),edges(),size(0){
 
             }
 
-            template <typename T>
-            Graph<T>::Graph(const std::vector<T>& nodes, const std::vector< std::pair<unsigned int, unsigned int> >& edges)
-            :nodes(nodes),edges(edges),size(nodes.size()){
+            // template <typename T>
+            // Graph<T>::Graph(const std::vector<T>& nodes, const std::vector< std::pair<unsigned int, unsigned int> >& edges)
+            // :nodes(nodes),edges(edges),size(nodes.size()){
 
-            }
+            // }
 
             //get函数
-            template <typename T>
-            std::vector<T>& Graph<T>::get_nodes(){
-                return nodes;
-            }
+            // template <typename T>
+            // std::vector<T>& Graph<T>::get_nodes(){
+            //     return nodes;
+            // }
             
-            template <typename T>
-            std::vector< std::pair<unsigned int, unsigned int> >& Graph<T>::get_edges(){
-                return edges;
-            }
+            // template <typename T>
+            // std::vector< std::pair<unsigned int, unsigned int> >& Graph<T>::get_edges(){
+            //     return edges;
+            // }
             
-            template <typename T>
-            unsigned int Graph<T>::get_size(){
-                return size;
-            }
+            // template <typename T>
+            // unsigned int Graph<T>::get_size(){
+            //     return size;
+            // }
 
             //专用函数
             //将节点数据转化成Eigen矩阵
-            template <typename T>
-            Eigen::MatrixXd Graph<T>::nodes_data_to_matrix(){
-                if(nodes.size() <= 0){
-                    std::cout << "no nodes data" << std::endl;
-                    return Eigen::MatrixXd(0,0);
-                }
-                int point_size = nodes[0].data.size();
-                Eigen::MatrixXd res(size, point_size);//返回结果
-                for(unsigned int i = 0; i < size; i++){
-                    for(unsigned int j = 0; j < point_size; j++){
-                        res(i,j) = nodes[i][j];
-                    }
-                }
-                return res;
-            }
+            // template <typename T>
+            // Eigen::MatrixXd Graph<T>::nodes_data_to_matrix(){
+            //     if(nodes.size() <= 0){
+            //         std::cout << "no nodes data" << std::endl;
+            //         return Eigen::MatrixXd(0,0);
+            //     }
+            //     int point_size = nodes[0].data.size();
+            //     Eigen::MatrixXd res(size, point_size);//返回结果
+            //     for(unsigned int i = 0; i < size; i++){
+            //         for(unsigned int j = 0; j < point_size; j++){
+            //             res(i,j) = nodes[i][j];
+            //         }
+            //     }
+            //     return res;
+            // }
         }
     }
 }
