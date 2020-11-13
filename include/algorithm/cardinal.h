bcc6e50a6b29fa938bc26ce49d9bc62d73840803
whwuhan <695549978@qq.com>
2020-11-13 20:41:38 +0800

    重要！！修改cardinal bug

--- include/algorithm/cardinal.h
+++ include/algorithm/cardinal.h
@@ -20,21 +20,22 @@ namespace wh{
         //==============算法实现===============
         template <typename T>
         std::vector<T> cardinal(std::vector<T> points, int inter_amount, double tightness){
+            std::vector<T> res;//返回的结果
             if(points.size() != 4){
                 std::cout << "wrong points" << std::endl;
-                return std::vector<T> (); 
+                return res;
             }
             //Cardinal样条插值
-            std::vector<T> res;//返回的结果
             double delta = (1.0 - tightness) / 2.0;//用tightness控制松紧程度
             for(int i = 0; i < inter_amount; i++){
-                double u = (i + 1) * 1.0 / (1.0 + inter_amount);
+                double u = (i + 1) / (1.0 + inter_amount);
+                std::cout << "u:" << u <<std::endl;
                 //计算插入点
                 T inter_point= \
                 points[0] * ((-delta * std::pow(u,3)) + 2 * delta * std::pow(u,2) - delta * u) +\
                 points[1] * ((2 - delta) * std::pow(u,3) + (delta - 3) * std::pow(u,2) + 1) +\
                 points[2] * ((delta - 2) * std::pow(u,3) + (3 - 2 * delta) * std::pow(u,2) + delta * u) +\
-                points[3] * (delta *  std::pow(u,3) - delta * std::pow(u,3));
+                points[3] * (delta * std::pow(u,3) - delta * std::pow(u,2));
                 res.push_back(inter_point);
             }
             return res;
