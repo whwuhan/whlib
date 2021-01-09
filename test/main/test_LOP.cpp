8c4f48710ecbd2b45a9065815512b348b3e4ee97
whwuhan <695549978@qq.com>
2021-01-09 13:13:21 +0800

    整理代码

--- test/main/test_LOP.cpp
+++ test/main/test_LOP.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <basic/cube.h>
 #include <utils/io/io_obj.h>
-#include <basic/abstract_data_type/octree.h>
+#include <basic/octree.h>
 #include <algorithm/LOP.h>
 #include <utils/string_lib.h>
 using namespace std;
 using namespace wh::basic;
-using namespace wh::basic::adt;
 using namespace wh::utils::io;
 using namespace wh::alg;
 using namespace Eigen;
