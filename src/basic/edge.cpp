a632db3e1527d432fd2d21d26d9956c3f86ee929
whwuhan <695549978@qq.com>
2021-05-22 17:43:57 +0800

    update

--- src/basic/edge.cpp
+++ src/basic/edge.cpp
@@ -5,26 +5,36 @@ Edge::Edge() : fir_ver_index(0), sec_ver_index(0){}
 
 Edge::Edge(unsigned int fir_ver_index, unsigned int sec_ver_index, bool unordered){
     //unordered:是否无序
-    if(unordered){
-        if (fir_ver_index < sec_ver_index){
+    if(unordered)
+    {
+        if (fir_ver_index < sec_ver_index)
+        {
             this->fir_ver_index = fir_ver_index;
             this->sec_ver_index = sec_ver_index;
         }else{
             this->fir_ver_index = sec_ver_index;
             this->sec_ver_index = fir_ver_index;
         }
-    }else{
+    }
+    else
+    {
         this->fir_ver_index = fir_ver_index;
         this->sec_ver_index = sec_ver_index;
     }
 }
 
-bool Edge::operator<(const Edge &edge)const{
-    if(fir_ver_index < edge.fir_ver_index){
+bool Edge::operator<(const Edge &edge)const
+{
+    if(fir_ver_index < edge.fir_ver_index)
+    {
         return true;
-    }else if(fir_ver_index == edge.fir_ver_index){
+    }
+    else if(fir_ver_index == edge.fir_ver_index)
+    {
         if(sec_ver_index < edge.sec_ver_index)
+        {
             return true;
+        }
     }
     return false;
 }
